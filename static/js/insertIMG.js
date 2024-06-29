bac8c072e068123ecbec4306b971200928c092de
tamocles <pitogomez28166561@gmail.com>
2024-06-29 03:08:55 +0200

    terceros cambios

--- static/js/insertIMG.js
+++ static/js/insertIMG.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+  const url = ["black.png","blue.png","white.png","yellow.png"];
+  const img = document.querySelectorAll(".insertImg");
+  const imgPersona = document.querySelector(".imgPersona");
   // Código a ejecutar cuando se haya cargado el HTML
  const container = document.querySelector(".container");
  container.style='background-image:url("/img/transfondo.png")'; 
+ imgPersona.src="/img/girls.png";
+  for(let x = 0 ; x < img.length;x++){
+      img[x].src=`/img/${url[x]}`;
+  }
 });
